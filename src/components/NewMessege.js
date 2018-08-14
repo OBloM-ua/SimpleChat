4dc51ff45472886d74800f80a5120f9fdbf0aa6d
Oleh Baidiuk <Oleh12@gmail.com>
2018-08-14 21:39:23 +0200

    add new functional

--- src/components/NewMessege.js
+++ src/components/NewMessege.js
@@ -4,16 +4,38 @@ import '../index.css';
 
 
 class NewMessege extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'hallo'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
+
         return (
             <div className="NewMessege">
-                <form>
+
+                <form onSubmit={this.handleSubmit}>
                     <label>
-                        New Messege:
-                        <input type="text" name="name" />
+                        Name:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Send" />
                 </form>
+
             </div>
         );
     }

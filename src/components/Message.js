7bc5de4636b6cb40ebdf7ed7dd9dc2c768d9297f
olehbaidiuk <oleh12@gmail.com>
2018-08-21 13:06:24 +0200

    create component Message

---
+++ src/components/Message.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import '../index.css';
+import PropTypes from 'prop-types';
+
+class Message extends Component {
+    propTypes = { //Перевіряє що об'єкт отримує
+        messages: PropTypes.object,
+    };
+
+    render() {
+
+        return (
+            <ul className="Message">
+                    {this.props.messages.map((item) => {
+                        return (
+                            <li key={item.id}>
+                                {item.user} <br/><b>{item.text}</b>
+                            </li>
+                        )
+                    })}
+                </ul>
+        );
+    }
+}
+
+export default Message;

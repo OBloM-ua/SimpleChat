09afb45771f584ae6090560cec291629b156e81d
olehbaidiuk <oleh12@gmail.com>
2018-08-22 14:11:05 +0200

    css verbesserungen

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -19,7 +19,6 @@ class AppHeader extends Component {
                     <div>
                         <li><a className="active" href="/">Chat</a></li>
                         <li><a href="https://github.com/OBloM-ua">My Git</a></li>
-                        <li><a href="/Contact">Contact</a></li>
                         <li className="rightOut" onClick={this.props.logOutAction}><a>LogOut</a></li>
                         <li className="rightOut">
                             <div className="PhotoURL" style={{ background: 'url(%1) no-repeat'.replace('%1', this.props.user.photoURL),
